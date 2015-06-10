ecc2614831c65e9f39797d748f66c3b5d09469bb
strever <qmailme@qq.com>
2015-06-10 15:00:52 +0800

    add some routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -19,3 +19,25 @@ Route::controllers([
 	'auth' => 'Auth\AuthController',
 	'password' => 'Auth\PasswordController',
 ]);
+
+//测试路由
+
+Route::get('test/{param?}', function($hello = 'hello') {
+    if($hello == 'hello') {
+        return $hello;
+    }
+    else {
+        return 'hello, ' . $hello;
+    }
+});
+
+Route::get('test/regexp/{paramInt}', function($int) {
+    return $int;
+})->where('paramInt', '[0-9]+');
+
+//使用?时_$pn都要去掉，不知为何
+Route::get('test/regexp/artist_{id}_{pn?}', function($id, $pn = 1) {
+    return 'artistId:' . $id . ' and pn:' . $pn;
+})->where(['id' => '[0-9]+', 'pn' => '[0-9]+']);
+
+
